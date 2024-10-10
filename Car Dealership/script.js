e08d0fcfee074722bef136f53854be97197da525
Prince  Daka <charlieissachar@gmail.com>
2024-10-10 16:03:50 +0200

    Add files via upload

---
+++ Car Dealership/script.js
@@ -0,0 +1,8 @@
+// JavaScript can be added here for additional interactivity
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Caspian Car Dealership website loaded successfully.");
+});
+// JavaScript can be added here for additional interactivity
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Caspian Car Dealership website loaded successfully.");
+});
